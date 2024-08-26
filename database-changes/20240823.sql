66880393605efcb70d0e3153e4af7a7917071872
VicGanuza <victoriaganuza@gmail.com>
2024-08-26 14:47:18 -0300

    eliminar Busq por id

--- database-changes/20240823.sql
+++ database-changes/20240823.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `busquedas_clientes` ADD `vendedor_id` INT NULL DEFAULT NULL AFTER `cliente_id`;
+ALTER TABLE `busquedas_clientes` ADD `id` INT NOT NULL FIRST;
+ALTER TABLE `busquedas_clientes` CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`id`);
